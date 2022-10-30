c7674eb4cf350928c68d8f6098bf5c7121e0965e
Aleksandr Tupitcyn <aleksandr.tupitcyn@mostgames.org>
2022-10-30 23:04:33 +0300

    Add project files.

---
+++ Raycaster2/Raycaster2.cpp
@@ -0,0 +1,12 @@
+﻿// Raycaster2.cpp : Defines the entry point for the application.
+//
+
+#include "Raycaster2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
